94d290531a952bf6811bd94f17e058b9a0d2806f
DevKang <kangyankun@gmail.com>
2017-01-31 22:50:05 +0800

    modify nginx server config

--- scripts/serve-laravel.sh
+++ scripts/serve-laravel.sh
@@ -23,9 +23,9 @@ block="server {
     index index.html index.htm index.php;
 
     charset utf-8;
-
+	autoindex on;
     location / {
-        try_files \$uri \$uri/ /index.php?\$query_string;
+        try_files \$uri \$uri/ /index.php?\$uri\$query_string;
     }
 
     location = /favicon.ico { access_log off; log_not_found off; }
@@ -38,13 +38,12 @@ block="server {
 
     client_max_body_size 100m;
 
-    location ~ \.php$ {
+    location ~ \.php {
         fastcgi_split_path_info ^(.+\.php)(/.+)$;
         fastcgi_pass unix:/var/run/php5-fpm.sock;
         fastcgi_index index.php;
         include fastcgi_params;
         fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
-
         fastcgi_intercept_errors off;
         fastcgi_buffer_size 16k;
         fastcgi_buffers 4 16k;
